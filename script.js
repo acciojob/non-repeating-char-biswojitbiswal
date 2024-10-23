dfb692b95ec41d43a04217e4439f60f31b999f95
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-23 12:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let currChar = {};
+
+	for(let char of str){
+		if(currChar[char] === undefined){
+			currChar[char] = 1;
+		} else {
+			currChar[char] += 1;
+		}
+	}
+
+	for(let char of str){
+		if(currChar[char] === 1){
+			return char;
+		}
+	}
+	return null;
+
 }
-const input = prompt("Enter a string");
+const input = prompt("Enter a string"); 
 alert(firstNonRepeatedChar(input)); 
